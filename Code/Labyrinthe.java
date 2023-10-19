a51ddd8e068c68350e71d5e72701a6642861848b
Moemen-02 <b8moemen@gmail.com>
2023-10-19 09:58:05 +0200

    Add Labyrinthe class and class digram

---
+++ Code/Labyrinthe.java
@@ -0,0 +1,28 @@
+package Code;
+import java.util.Random;
+
+public class Labyrinthe {
+    private int width;
+    private int height;
+    char[][] grid;
+
+    public Labyrinthe(int width, int height) {
+        this.width = width;
+        this.height = height;
+        this.grid = new char[width][height];
+        initializeLabyrinth();
+    }
+
+    private void initializeLabyrinth() {
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                if (i == 0 || i == width - 1 || j == 0 || j == (height - 1))
+                    grid[i][j] = 'X';
+                else
+                    grid[i][j] = ' ';
+            }
+        }
+    }
+
+
+}
---
+++ Code/main.java
@@ -0,0 +1,13 @@
+package Code;
+import java.util.ArrayList;
+public class main {
+    public static void main(String[] args){
+        Labyrinthe L = new Labyrinthe(5,5);
+
+        System.out.print(L.grid[4][4]);
+        System.out.print(L.grid[1][0]);
+
+
+
+    }
+}
