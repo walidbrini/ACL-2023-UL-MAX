bda52210f8939756062c33a36bbfbe32b40b49b3
Moemen-02 <b8moemen@gmail.com>
2023-12-04 22:32:25 +0100

    Update pom.xml and Add LybyrinthTest class

---
+++ Code/game/src/test/LabyrinthTest.java
@@ -0,0 +1,32 @@
+import com.example.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class LabyrinthTest {
+    private Labyrinth labyrinth;
+/*
+    @BeforeEach
+    void setUp() {
+        labyrinth = new Labyrinth(16, 16, new GamePanel());
+    }
+
+    @Test
+    void testRightResults() {
+        labyrinth.generate(null);
+        assertNotNull(labyrinth.getTreasure());
+        assertNotNull(labyrinth.getSpawn());
+        assertNotNull(labyrinth.getGrid());
+    }
+
+    @Test
+    void testBoundaryConditions() {
+        // Test with the smallest possible labyrinth
+        Labyrinth smallLabyrinth = new Labyrinth(1, 1, new GamePanel());
+        smallLabyrinth.generate(null);
+        assertNotNull(smallLabyrinth.getTreasure());
+        assertNotNull(smallLabyrinth.getSpawn());
+        assertNotNull(labyrinth.getGrid());
+    }
+*/
+}
