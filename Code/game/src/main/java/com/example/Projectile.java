8139ec3c006dd67f224b345e196266018b62c6cd
Othmen Derouiche <othmenderouiche@gmail.com>
2023-12-04 21:54:23 +0100

    add projectile class

---
+++ Code/game/src/main/java/com/example/Projectile.java
@@ -0,0 +1,14 @@
+package com.example;
+public class Projectile extends Entity {
+    public Projectile(GamePanel gp){
+        super(gp);
+    }
+
+    public void set(){
+
+    }
+    public void update(){
+
+    }
+
+}
